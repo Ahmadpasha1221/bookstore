dc267d659e7eda88359f895c86719071d02a9053
Ahmadpasha1221 <ahmadpashashaiks@gmail.com>
2025-02-25 00:22:18 +0530

    fix: changed store.js to Store.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.jsx";
 import { Provider } from "react-redux";
-import Store from "./redux/store";
+import Store from "./redux/Store.js";
 import "sweetalert2/dist/sweetalert2.js";
 createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
